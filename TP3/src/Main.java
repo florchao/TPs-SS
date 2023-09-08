00599e5d02585f26ed2b50e4d5a5b3d32844b099
florencia chao <fchao@itba.edu.ar>
2023-09-08 12:31:37 -0300

    read and generate file

--- TP3/src/Main.java
+++ TP3/src/Main.java
@@ -1,19 +1,45 @@
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+
     public static int N;
     public static double L;
     private static Set<Particle> particles = new HashSet<>();
 
-    public static void main(String[] args) {
+    private static void generateParticles(FileWriter inputFile) throws IOException {
+        double weight = 1.00;
+        double radius = 0.0015;
+        double v = 0.01;
+        double length = 0.09;
+        inputFile.write("   " + N);
+
+        double rx, ry, theta, vx, vy;
+        for (int i = 0; i < N; i++) {
+            rx = length * Math.random();
+            ry = length * Math.random();
+            theta = 2 * Math.PI * Math.random();
+            vx = v * Math.cos(theta);
+            vy = v * Math.sin(theta);
+
+            inputFile.write("\n   " + rx + "    " + ry + "    " + vx + "    " + vy + "    " + weight + "    " + radius);
+        }
+        inputFile.close();
+    }
+
+    public static void main(String[] args) throws IOException {
 
         File inputFile = new File(args[0]);
+        FileWriter staticWriter = new FileWriter("./output/static.txt");
+
+        generateParticles(staticWriter);
 
         List<String> data;
         try {
